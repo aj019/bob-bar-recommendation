2c4bc565135dab5d0d49a5e16b277e1f81be4589
Anuj Gupta <anujgupta019@gmail.com>
2025-04-19 16:10:50 +0530

    Made user data dynamic

--- src/services.ts
+++ src/services.ts
@@ -87,95 +87,95 @@ export interface BarData {
 }
 
 export const fetchUserBarData = async (username: string): Promise<BarData> => {
-  // const response = await axios.get(`https://services.baxus.co/api/bar/user/${username}`, {
-  //   headers: {
-  //     'Access-Control-Allow-Origin': '*'
-  //   }
-  // });
+  const response = await axios.get(`https://services.baxus.co/api/bar/user/${username}`, {
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  });
   // console.log(response);
   // Add check for response status
   // if (response.status !== 200) {
-  const sampleResponse = [
-    {
-        "id": 1030823,
-        "bar_id": 1030823,
-        "price": null,
-        "note": null,
-        "created_at": "2024-03-14T22:47:31.183Z",
-        "updated_at": "2024-08-26T19:11:25.442Z",
-        "user_id": 100248,
-        "release_id": 13266,
-        "fill_percentage": 0,
-        "added": "2024-03-12T00:00:00.000Z",
-        "user": {
-            "user_name": "carriebaxus"
-        },
-        "product": {
-            "id": 13266,
-            "name": "Heaven Hill Bottled In Bond 7 Year",
-            "image_url": "https://d1w35me0y6a2bb.cloudfront.net/newproducts/recSJfTSxTvljLvF8",
-            "brand_id": 430,
-            "brand": "Heaven Hill",
-            "spirit": "Bourbon",
-            "size": "750",
-            "proof": 100,
-            "average_msrp": 39.99,
-            "fair_price": 41.79,
-            "shelf_price": 47.19,
-            "popularity": 100083,
-            "created": 1606080229977,
-            "updated": 1744906420523,
-            "barcode": "096749002429,096749004942,093799006049",
-            "barrel_pick": false,
-            "user_added_id": 140857,
-            "submitter_email": "info@baxus.co",
-            "submitter_username": "baxustest",
-            "private": false,
-            "verified_date": 1710970749000,
-            "user_added": false
-        }
-    },
-    {
-        "id": 1007256,
-        "bar_id": 1007256,
-        "price": null,
-        "note": "",
-        "created_at": "2024-03-05T14:21:37.321Z",
-        "updated_at": "2025-01-24T02:06:45.915Z",
-        "user_id": 100248,
-        "release_id": 24961,
-        "fill_percentage": 0,
-        "added": "2024-03-05T00:00:00.000Z",
-        "user": {
-            "user_name": "carriebaxus"
-        },
-        "product": {
-            "id": 24961,
-            "name": "Rare Perfection 14 Year",
-            "image_url": "https://d1w35me0y6a2bb.cloudfront.net/newproducts/247ad792-6d80-4d4d-92f6-4789183cd2f0",
-            "brand_id": 2827,
-            "brand": "Rare Perfection",
-            "spirit": "Canadian Whisky",
-            "size": "750",
-            "proof": 100.7,
-            "average_msrp": 160,
-            "fair_price": 164.85,
-            "shelf_price": 179.39,
-            "popularity": 0,
-            "created": 1709648481347,
-            "updated": 1744906210294,
-            "barcode": "852543006311",
-            "barrel_pick": false,
-            "user_added_id": 100248,
-            "submitter_email": "carrie@baxus.co",
-            "submitter_username": "carriebaxus",
-            "private": false,
-            "verified_date": 1721925966000,
-            "user_added": false
-        }
-    }
-]
-
+//   const sampleResponse = [
+//     {
+//         "id": 1030823,
+//         "bar_id": 1030823,
+//         "price": null,
+//         "note": null,
+//         "created_at": "2024-03-14T22:47:31.183Z",
+//         "updated_at": "2024-08-26T19:11:25.442Z",
+//         "user_id": 100248,
+//         "release_id": 13266,
+//         "fill_percentage": 0,
+//         "added": "2024-03-12T00:00:00.000Z",
+//         "user": {
+//             "user_name": "carriebaxus"
+//         },
+//         "product": {
+//             "id": 13266,
+//             "name": "Heaven Hill Bottled In Bond 7 Year",
+//             "image_url": "https://d1w35me0y6a2bb.cloudfront.net/newproducts/recSJfTSxTvljLvF8",
+//             "brand_id": 430,
+//             "brand": "Heaven Hill",
+//             "spirit": "Bourbon",
+//             "size": "750",
+//             "proof": 100,
+//             "average_msrp": 39.99,
+//             "fair_price": 41.79,
+//             "shelf_price": 47.19,
+//             "popularity": 100083,
+//             "created": 1606080229977,
+//             "updated": 1744906420523,
+//             "barcode": "096749002429,096749004942,093799006049",
+//             "barrel_pick": false,
+//             "user_added_id": 140857,
+//             "submitter_email": "info@baxus.co",
+//             "submitter_username": "baxustest",
+//             "private": false,
+//             "verified_date": 1710970749000,
+//             "user_added": false
+//         }
+//     },
+//     {
+//         "id": 1007256,
+//         "bar_id": 1007256,
+//         "price": null,
+//         "note": "",
+//         "created_at": "2024-03-05T14:21:37.321Z",
+//         "updated_at": "2025-01-24T02:06:45.915Z",
+//         "user_id": 100248,
+//         "release_id": 24961,
+//         "fill_percentage": 0,
+//         "added": "2024-03-05T00:00:00.000Z",
+//         "user": {
+//             "user_name": "carriebaxus"
+//         },
+//         "product": {
+//             "id": 24961,
+//             "name": "Rare Perfection 14 Year",
+//             "image_url": "https://d1w35me0y6a2bb.cloudfront.net/newproducts/247ad792-6d80-4d4d-92f6-4789183cd2f0",
+//             "brand_id": 2827,
+//             "brand": "Rare Perfection",
+//             "spirit": "Canadian Whisky",
+//             "size": "750",
+//             "proof": 100.7,
+//             "average_msrp": 160,
+//             "fair_price": 164.85,
+//             "shelf_price": 179.39,
+//             "popularity": 0,
+//             "created": 1709648481347,
+//             "updated": 1744906210294,
+//             "barcode": "852543006311",
+//             "barrel_pick": false,
+//             "user_added_id": 100248,
+//             "submitter_email": "carrie@baxus.co",
+//             "submitter_username": "carriebaxus",
+//             "private": false,
+//             "verified_date": 1721925966000,
+//             "user_added": false
+//         }
+//     }
+// ]
+  const sampleResponse = response.data
     // Convert sample response to Bottle[] only picking out the product fields
     const bottles = sampleResponse.map(item => {
       return {
