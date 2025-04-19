206ba8d9baab76529e075eed4830fe0f084c1c5b
Anuj Gupta <anujgupta019@gmail.com>
2025-04-19 16:07:47 +0530

    Testing sample user data

--- src/services.ts
+++ src/services.ts
@@ -65,7 +65,7 @@ const findSimilarBottles = async (userBottle: Bottle, allBottles: Bottle[], limi
 export interface Bottle {
   id: number;
   name: string;
-  size: number;
+  size: number|string;
   proof: number | null;
   abv: number | null;
   spirit_type: string;
@@ -95,25 +95,110 @@ export const fetchUserBarData = async (username: string): Promise<BarData> => {
   // console.log(response);
   // Add check for response status
   // if (response.status !== 200) {
-    return { bottles: [{
-      id: 24961,
-      name: "Rare Perfection 14 Year",
-      image_url: "https://d1w35me0y6a2bb.cloudfront.net/newproducts/247ad792-6d80-4d4d-92f6-4789183cd2f0",
-      brand_id: 2827,
-      spirit_type: "Canadian Whisky",
-      size: 750,
-      proof: 100.7,
-      abv: 50.35,
-      avg_msrp: 160,
-      fair_price: 164.85,
-      shelf_price: 179.39,
-      popularity: 0,
-      total_score: 0,
-      wishlist_count: 0,
-      vote_count: 0,
-      bar_count: 0,
-      ranking: 0
-    }] };
+  const sampleResponse = [
+    {
+        "id": 1030823,
+        "bar_id": 1030823,
+        "price": null,
+        "note": null,
+        "created_at": "2024-03-14T22:47:31.183Z",
+        "updated_at": "2024-08-26T19:11:25.442Z",
+        "user_id": 100248,
+        "release_id": 13266,
+        "fill_percentage": 0,
+        "added": "2024-03-12T00:00:00.000Z",
+        "user": {
+            "user_name": "carriebaxus"
+        },
+        "product": {
+            "id": 13266,
+            "name": "Heaven Hill Bottled In Bond 7 Year",
+            "image_url": "https://d1w35me0y6a2bb.cloudfront.net/newproducts/recSJfTSxTvljLvF8",
+            "brand_id": 430,
+            "brand": "Heaven Hill",
+            "spirit": "Bourbon",
+            "size": "750",
+            "proof": 100,
+            "average_msrp": 39.99,
+            "fair_price": 41.79,
+            "shelf_price": 47.19,
+            "popularity": 100083,
+            "created": 1606080229977,
+            "updated": 1744906420523,
+            "barcode": "096749002429,096749004942,093799006049",
+            "barrel_pick": false,
+            "user_added_id": 140857,
+            "submitter_email": "info@baxus.co",
+            "submitter_username": "baxustest",
+            "private": false,
+            "verified_date": 1710970749000,
+            "user_added": false
+        }
+    },
+    {
+        "id": 1007256,
+        "bar_id": 1007256,
+        "price": null,
+        "note": "",
+        "created_at": "2024-03-05T14:21:37.321Z",
+        "updated_at": "2025-01-24T02:06:45.915Z",
+        "user_id": 100248,
+        "release_id": 24961,
+        "fill_percentage": 0,
+        "added": "2024-03-05T00:00:00.000Z",
+        "user": {
+            "user_name": "carriebaxus"
+        },
+        "product": {
+            "id": 24961,
+            "name": "Rare Perfection 14 Year",
+            "image_url": "https://d1w35me0y6a2bb.cloudfront.net/newproducts/247ad792-6d80-4d4d-92f6-4789183cd2f0",
+            "brand_id": 2827,
+            "brand": "Rare Perfection",
+            "spirit": "Canadian Whisky",
+            "size": "750",
+            "proof": 100.7,
+            "average_msrp": 160,
+            "fair_price": 164.85,
+            "shelf_price": 179.39,
+            "popularity": 0,
+            "created": 1709648481347,
+            "updated": 1744906210294,
+            "barcode": "852543006311",
+            "barrel_pick": false,
+            "user_added_id": 100248,
+            "submitter_email": "carrie@baxus.co",
+            "submitter_username": "carriebaxus",
+            "private": false,
+            "verified_date": 1721925966000,
+            "user_added": false
+        }
+    }
+]
+
+    // Convert sample response to Bottle[] only picking out the product fields
+    const bottles = sampleResponse.map(item => {
+      return {
+        id: item.product.id,
+        name: item.product.name,
+        image_url: item.product.image_url,
+        brand_id: item.product.brand_id,
+        spirit_type: item.product.spirit,
+        size: item.product.size,
+        proof: item.product.proof,
+        abv: item.product.proof ? item.product.proof / 2 : null,
+        avg_msrp: item.product.average_msrp,
+        fair_price: item.product.fair_price,
+        shelf_price: item.product.shelf_price,
+        popularity: item.product.popularity,
+        total_score: 0,
+        wishlist_count: 0,
+        vote_count: 0,
+        bar_count: 0,
+        ranking: 0
+      }
+    });
+    return { bottles };
   // }
   // return response.data;
 };
